1339413a1e4c66d84f7485d3eea28eeb82a11a31
darshit <hairiyadarshit@gmail.com>
2024-07-26 23:41:12 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import { login,logout } from './store/authSlice'
+import {Header,Footer} from './components'
+import {Outlet} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+
+      if (userData){
+        dispatch(login(userData)) // {userData}
+      }
+      else{
+        dispatch(logout())
+      }
+
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-[#e79b29]'>
+      <div className='w-full block'>
+        <Header />
+
+        <main>
+          <Outlet />
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import { Container, Logo, LogoutBtn } from '../'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+// #0e062e #130552
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [mobile, setMobile] = useState(false);
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    }
+  ]
+
+  return (
+    <header className='py-3 font-medium shadow text-[#cccccc] bg-[#040404] sticky'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo className='bg-cover min-w-12' />
+            </Link>
+          </div>
+          <button
+            className={`sm:hidden flex ml-auto`}
+            onClick={() => {
+              setMobile(!mobile)
+            }}
+          >
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png" alt="Menu" className='bg-cover w-[3rem]' />
+          </button>
+        
+          <ul className={'hidden sm:flex ml-auto'} >
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200  rounded-full'   //hover:bg-[#cccccc]
+                  >{item.name}</button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        {mobile && (
+            <ul className={`absolute z-30 right-0 mt-2 py-2 w-48 text-[#988dcc] bg-[#0e062e] rounded-lg shadow-xl`} >
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-[#032372] rounded-full'
+                  >{item.name}</button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+            </ul>
+          )}
+      </Container>
+    </header>
+  )
+}
+
+export default Header
